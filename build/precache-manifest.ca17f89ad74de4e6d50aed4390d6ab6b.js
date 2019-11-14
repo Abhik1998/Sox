5faa45dad0dde09b8518e84ebc8d8b7b5dc10487
Abhik1998 <abhik1998chakraborty@gmail.com>
2019-11-14 22:07:56 +0530

    Added deployable files

---
+++ build/precache-manifest.ca17f89ad74de4e6d50aed4390d6ab6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b506a1050fd10117905365b993ba22a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dbb7f78c6088241ea61",
+    "url": "/static/css/2.44476ee4.chunk.css"
+  },
+  {
+    "revision": "1959e360fd3812789c6b",
+    "url": "/static/css/main.b4d72a7d.chunk.css"
+  },
+  {
+    "revision": "8dbb7f78c6088241ea61",
+    "url": "/static/js/2.0b3232d2.chunk.js"
+  },
+  {
+    "revision": "1959e360fd3812789c6b",
+    "url": "/static/js/main.9d77ce55.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
